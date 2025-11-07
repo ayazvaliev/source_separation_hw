5092cda186a0f53c08cb2f8b0954773c8e7ec243
Ayaz <valiewayaz2003@gmail.com>
2025-11-07 08:39:33 +0300

    minor fix

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -95,11 +95,11 @@ class MainDataset(BaseDataset):
 
             data_instance = OrderedDict()
 
-            for path, name in zip(["audio_mix_path", "audio_s1_path", "audio_s2_path"], 
+            for path, name in zip(["audio_mix", "audio_s1", "audio_s2"], 
                                   [audio_mix_path, audio_s1_path, audio_s2_path]):
                 if os.path.exists(path):
                     info = torchaudio.info(audio_mix_path)
-                    data_instance[name] = path
+                    data_instance[name + "_path"] = path
                     data_instance[name + "_time"] = info.num_frames / info.sample_rate
 
             data_instance["mouths_path"] = str(mouths_path / item_name)
