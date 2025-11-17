6f27fed8dc452635009682991c19aa92fff4e215
Ayaz <valiewayaz2003@gmail.com>
2025-11-17 12:22:33 +0300

    debug

--- src/trainer/base_trainer.py
+++ src/trainer/base_trainer.py
@@ -596,6 +596,7 @@ class BaseTrainer:
             n_infs = int(torch.isinf(sd).sum().item())
             bad_tensors.append((key, "-", sd.shape, sd.dtype, n_nans, n_infs))
         else:
+            print(key, sd)
             for k, t in sd.items():
                 if not torch.is_tensor(t):
                     continue
