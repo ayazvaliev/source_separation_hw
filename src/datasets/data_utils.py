e7e8505f8ba56b1579dcc8fbb118cb10df6b0e3f
Ayaz <valiewayaz2003@gmail.com>
2025-11-17 22:38:12 +0300

    added separate val dataloader config

--- src/datasets/data_utils.py
+++ src/datasets/data_utils.py
@@ -74,14 +74,14 @@ def get_dataloaders(config, device):
     for dataset_partition in config.datasets.keys():
         dataset = datasets[dataset_partition]
 
-        assert dataset_partition != "train" or config.dataloader.batch_size <= len(dataset), (
+        assert dataset_partition != "train" and config.dataloader["inference"].batch_size <= len(dataset), (
             f"The batch size ({config.dataloader.batch_size}) cannot "
             f"be larger than the dataset length ({len(dataset)})"
         )
-
+        dataloader_config = config.dataloader["train"] if dataset_partition == "train" else config.dataloader["inference"] 
         max_workers = os.cpu_count()
         partition_dataloader = instantiate(
-            config.dataloader,
+            dataloader_config,
             dataset=dataset,
             collate_fn=collate_fn,
             drop_last=(dataset_partition == "train"),
