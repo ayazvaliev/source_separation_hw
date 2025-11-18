404b5450d1220302f864c2f3896da13de6dfad4f
Ayaz <valiewayaz2003@gmail.com>
2025-11-19 00:44:49 +0300

    added complex stft in transforms

---
+++ src/transforms/spec_augs/complex_spec.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torchaudio.transforms import Spectrogram
+
+
+class ComplexSpec(nn.Module):
+    def __init__(self, n_fft: int, win_length: int, hop_length: int, center: bool,  **kwargs):
+        super().__init__()
+        self.spec_transform = Spectrogram(
+            n_fft=n_fft,
+            win_length=win_length,
+            hop_length=hop_length,
+            power=None,
+            center=center
+        )
+
+    def __call__(self, audio: torch.Tensor) -> torch.Tensor:
+        complex_spec = self.spec_transform(audio)
+        return torch.stack([torch.real(complex_spec), torch.imag(complex_spec)], dim=1)
