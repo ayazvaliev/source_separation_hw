4fa09332cf7d8cb8b508195ad258b138ab77faab
Ayaz <valiewayaz2003@gmail.com>
2025-11-22 16:20:31 +0300

    twek weight init

--- src/model/moduls.py
+++ src/model/moduls.py
@@ -28,8 +28,8 @@ class PositionalEncoding(nn.Module):
 class GLN(nn.Module):
     def __init__(self, input_channel):
         super().__init__()
-        self.mean = nn.Parameter(torch.ones(input_channel), requires_grad=True)
-        self.var = nn.Parameter(torch.zeros(input_channel), requires_grad=True)
+        self.mean = nn.Parameter(torch.zeros(input_channel), requires_grad=True)
+        self.var = nn.Parameter(torch.mean(input_channel), requires_grad=True)
     
     def forward(self, x: torch.Tensor):
         # x [B, F, T]
