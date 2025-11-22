6d26dd25664ba97925f870fed119e91232348bb9
RavenGnaes <vgbyvgb@gmail.com>
2025-11-22 23:06:45 +0300

    Рабочая версия модели + тестер который показывает что она работает

--- Tester.py
+++ Tester.py
@@ -1,28 +1,22 @@
-import torchaudio
+from src.model.rtfsnet import RTFSNet
 import torch
-from pathlib import Path
-torchaudio.set_audio_backend("soundfile") 
-import warnings
-
-import hydra
-import torch
-from hydra.utils import instantiate
-from omegaconf import OmegaConf
-
-from src.datasets.data_utils import get_dataloaders
-from src.trainer import Trainer
-from src.utils.init_utils import set_random_seed, setup_saving_and_logging
 
 
-@hydra.main(version_base=None, config_path="src/configs", config_name="baseline")
-def main(config):
-    conf = config.feature_gainer["preprocessing_methods"]
-    
-    for name in conf.keys():
-        print("\n","\n","\n",name,"\n","\n","\n","\n")
-        func = instantiate(conf[name])
-        path = "E:\\Python_DLA_proj\\Speach_sep\\pytorch_project_template\\data\\example\\train\\dla_dataset\\audio\\train\\mix\\00000002026_00315409549.wav"
-        data_object, sample_rate = torchaudio.load(path)
-        print(func(data_object).shape)
 
-main()
+
+
+block = RTFSNet(
+    n_feats = 1024//2+1,        
+    n_fft = 1024,         
+    hop_length = 256,     
+    win_length = 1024 ,
+    time_dim = 126   
+)
+
+video_1 = torch.randn(3, 50, 512)
+video_2 = torch.randn(3, 50, 512)
+Audio_1 = torch.randn(3, 32000)
+
+
+res = block(Audio_1, video_1, video_2)
+print( res["logits"].shape)
