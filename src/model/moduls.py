6516a72bfad41956d452011f614a8a3b36a210ac
Ayaz <valiewayaz2003@gmail.com>
2025-11-22 16:56:52 +0300

    reduced number of blocks

--- src/model/moduls.py
+++ src/model/moduls.py
@@ -175,14 +175,18 @@ class TransformerLayer(nn.Module):
                  conv_kernel_size,
                  conv_stride,
                  conv_dilation,
-                 conv_padding
+                 conv_padding,
+                 ffn_only
     ):
         super().__init__()
         self.pos_encoder = PositionalEncoding(input_channel, time_dim)
-        self.mhsa = MHSA(embed_dim=input_channel, 
-                         nhead=mhsa_nhead, 
-                         dropout=mhsa_dropout
-                        )
+        if not ffn_only:
+            self.mhsa = MHSA(embed_dim=input_channel, 
+                            nhead=mhsa_nhead, 
+                            dropout=mhsa_dropout
+                            )
+        else:
+            self.mhsa = nn.Identity()
         self.ffn = FFN(
             input_channel=input_channel,
             kernel_size=conv_kernel_size,
