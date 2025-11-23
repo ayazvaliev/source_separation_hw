c1ca6362c55306b520ef1e0b556f1a458bc5fb1c
Ayaz <valiewayaz2003@gmail.com>
2025-11-23 17:28:43 +0300

    bring back old ffnonly

--- src/model/moduls.py
+++ src/model/moduls.py
@@ -179,13 +179,12 @@ class TransformerLayer(nn.Module):
                  ffn_only
     ):
         super().__init__()
-        self.ffn_only = ffn_only
+        self.pos_encoder = PositionalEncoding(input_channel, time_dim)
         if not ffn_only:
             self.mhsa = MHSA(embed_dim=input_channel, 
                             nhead=mhsa_nhead, 
                             dropout=mhsa_dropout
                             )
-            self.pos_encoder = PositionalEncoding(input_channel, time_dim)
         else:
             self.mhsa = nn.Identity()
         self.ffn = FFN(
@@ -198,8 +197,7 @@ class TransformerLayer(nn.Module):
         )
     
     def forward(self, x: torch.Tensor):
-        if not self.ffn_only:
-            x = x + self.mhsa(self.pos_encoder(x))
+        x = x + self.mhsa(self.pos_encoder(x))
         return x + self.ffn(x)
 
 
